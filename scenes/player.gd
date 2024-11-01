0e0ee9c9bc0bb3bf5d3c5533df53446ccd4c9420
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2024-11-01 12:01:12 -0300

    .

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @export var walk_speed = 4.0
+@onready var anim_tree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
+
 const TILE_SIZE = 16
 
 var initial_position = Vector2(0, 0)
@@ -14,21 +17,29 @@ func _ready():
 
 func _physics_process(delta):
 	if is_moving == false:
-		process_player_input() # Verifique se essa função está implementada corretamente
+		process_player_movement_input() # Verifique se essa função está implementada corretamente
 	elif input_direction != Vector2.ZERO:
+		anim_state.travel("Walk")
 		move(delta) # Verifique se essa função move() existe e está implementada corretamente
 	else:
+		anim_state.travel("Idle")
 		is_moving = false
 
-func process_player_input():
+func process_player_movement_input():
 	if input_direction.y == 0:
 		input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	if input_direction.x == 0:
 		input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 
 	if input_direction != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_direction)
+		anim_tree.set("parameters/Walk/blend_position", input_direction)
 		initial_position = position
 		is_moving = true
+	else:
+		anim_state.travel("Idle")
+
+
 		
 func move(delta):
 	percent_moved_to_next_tile += walk_speed * delta
